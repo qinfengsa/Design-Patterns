32af59f83c28601095297538c8818e4feab165d0
qinfengsama <42572617+qinfengsama@users.noreply.github.com>
2019-03-18 00:45:29 +0800

    原型模式作业

---
+++ src/main/java/com/qinfengsa/pattern/prototype/homework/OrderService.java
@@ -0,0 +1,10 @@
+package com.qinfengsa.pattern.prototype.homework;
+
+/**
+ * @author: qinfengsa
+ * @date: 2019/3/17 23:12
+ */
+public interface OrderService {
+
+    void createOrderByJson(String json) throws Exception;
+}
